cfd055b7e6ea719ba2c2064b1b0189572911dd6b
JoeyC <55225247+JoeC-Dev@users.noreply.github.com>
2022-07-26 20:33:19 -0400

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -4,6 +4,6 @@ set -x
 sudo apt update
 sudo apt install -y nfs-common
 
-mkdir webserver_log
+mkdir webserver_monitor
 sleep 1m
-sudo mount 192.168.1.1:/share/log webserver_log/
+sudo mount 192.168.1.1:/var/webserver_log webserver_monitor/
