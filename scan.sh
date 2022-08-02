2a0064bad54f70bc33af1f5348396af5e752e043
JoeyC <55225247+JoeC-Dev@users.noreply.github.com>
2022-08-01 22:53:34 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,9 +1,20 @@
-#!/bin/bash 
+#!/bin/bash
 
-full=$(sudo grep -E -i 'invalid' /var/log/auth.log | grep from | cut -d ' ' -f 1,2,10 )
-echo $full > temp
-ip=$(cut -d ' ' -f 3 temp)
-country=$(geoiplookup $ip)
-echo $full $country >> /var/webserver_log/unaothorized.log
-rm temp
-sudo rm /var/log/auth.log
+sudo grep -i -E 'sshd' /var/log/auth.log | grep Received | cut -d ' ' -f 1,2,3,10 > temp
+
+while IFS= read -r line; do arr+=("$line");done <temp
+
+sudo touch /var/webserver_log/unauthorized.log
+
+for value in "${arr[@]}"; do echo $value>temp2;date=$(cut -d ' ' -f 1,2 temp2); ip=$(cut -d ' ' -f 3 temp2);country=$(geoiplookup $ip); echo "IP:" $ip " ||| COUNTRY:" $country " ||| DATE:" $date >> temp3; done
+
+sudo cp temp3 /var/webserver_log/unauthorized.log
+
+rm temp2 temp3
+
+# NEED TO ONLY DO THIS ONCE WHEN BOOTING MIGHT BE ABLE TO PUT IT IN SETUP APACHE 
+#echo "*/5 * * * * bash /local/repository/scan.sh" > temp5
+
+#sudo crontab temp5
+
+#sudo cat /var/webserver_log/unauthorized.log
