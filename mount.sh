9a662b3506a185ac979e57ec3b6332f1d888694a
JoeyC <55225247+JoeC-Dev@users.noreply.github.com>
2022-08-03 07:46:47 -0400

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -8,7 +8,7 @@ sudo mkdir -p /var/webserver_monitor/
 sleep 1m
 sudo mount 192.168.1.1:/var/webserver_log /var/webserver_monitor
 
-echo "MAILTO="joeburn309@gmail.com"" >> temp
+echo "MAILTO=\"joeburn309@gmail.com\"" >> temp
 echo "*/60 * * * * bash /local/repository/monitor.sh" >> temp
 crontab temp 
 rm temp
