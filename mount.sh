0ab7fc336edc0816121563d2288abaff31d06d48
JoeyC <55225247+JoeC-Dev@users.noreply.github.com>
2022-08-02 20:09:52 -0400

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -7,3 +7,7 @@ sudo apt install -y nfs-common
 sudo mkdir -p /var/webserver_monitor/
 sleep 1m
 sudo mount 192.168.1.1:/var/webserver_log /var/webserver_monitor
+
+echo "*/60 * * * * bash /local/repository/monitor.sh" > temp
+crontab temp 
+rm temp
