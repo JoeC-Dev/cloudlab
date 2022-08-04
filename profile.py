9a0b2b78bae6a43c94aae1f57f92f6bc96812533
JoeyC <55225247+JoeC-Dev@users.noreply.github.com>
2022-08-03 21:40:53 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -25,7 +25,7 @@ for i in range(3):
   
   if i == 0:
     node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_apache.sh"))
-  else if i == 1:
+  elif i == 1:
     node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/mount.sh"))
   else: 
     node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/ldap.sh"))
