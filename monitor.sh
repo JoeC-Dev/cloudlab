98c4cefcba2c0c13465b22f623cc49ac93de932a
JoeyC <55225247+JoeC-Dev@users.noreply.github.com>
2022-08-04 22:46:01 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -8,4 +8,5 @@ echo "No unauthorized access"
 fi
 
 
-sudo tail -n 1 /var/webserver_monitor/unauthorized.log > /var/webserver_monitor/last.log
+sudo tail -n 1 /var/webserver_monitor/unauthorized.log > temp2
+sudo cp temp2 /var/webserver_monitor/last.log
